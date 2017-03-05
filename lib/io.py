047c6ec337ca5bd4ddf788d3e1bef3fed16ae476
bogdanduminyuk <77934329bogdan@gmail.com>
2017-03-05 22:32:56 +0200

    Добавлены описательные комментарии в ExcelManager.

--- lib/io.py
+++ lib/io.py
@@ -17,6 +17,13 @@ class ExcelManager:
         self.sheets = None
 
     def open(self, filename, sheet_index):
+        """
+        Opens filename. Gets necessary info and keeps it in self.vars.
+
+        :param filename: name of the excel book
+        :param sheet_index: sheet number in the book
+        :return: None
+        """
         self.rb = xlrd.open_workbook(filename, formatting_info=True)
         self.sheets = self.rb.sheets()
         self.wb = copy.copy(self.rb)
@@ -24,6 +31,15 @@ class ExcelManager:
         self.w_sheet = self.wb.get_sheet(sheet_index)
 
     def get_array(self, start_cell, end_cell):
+        """
+        Returns numpy matrix of the given cells.
+
+        Cells should be Excel cells (e.g. A1, B3... etc).
+
+        :param start_cell: left top matrix cell
+        :param end_cell: right bottom matrix cell
+        :return: numpy matrix
+        """
         start_row, start_col = functions.xl_cell_to_row_col(start_cell)
         end_row, end_col = functions.xl_cell_to_row_col(end_cell)
 
@@ -40,6 +56,13 @@ class ExcelManager:
         return np.matrix(data)
 
     def append_matrix(self, matrix, start_cell):
+        """
+        Appends matrix to the input book.
+
+        :param matrix: matrix that will be appended
+        :param start_cell: left top cell position in given sheet
+        :return: None
+        """
         start_row, start_col = functions.xl_cell_to_row_col(start_cell)
         style = xlwt.XFStyle()
         style.num_format_str = '0.00'
@@ -49,4 +72,10 @@ class ExcelManager:
                 self.w_sheet.write(row_num, col_num, float(col_element), self.format)
 
     def save(self, filename):
+        """
+        Saves the current workbook to the given filename.
+
+        :param filename: path where workbook will be saved
+        :return: None
+        """
         self.wb.save(filename)
